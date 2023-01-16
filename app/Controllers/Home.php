cb94b0c9e0a0c08a1ccbce30d0d206b3953d5321
Pierre Blarre <pierre.blarre@gmail.com>
2023-01-16 20:55:03 +0100

    Exercice 6 - Retour sur le Controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,8 +1,14 @@
 <?php
+
+require_once 'app/Models/Article.php';
+
 class Home
 {
   function index()
   {
+    $article_model = new Article();
+
+    $articles = $article_model->list();
     require "resources/views/template.php";
   }
 }
